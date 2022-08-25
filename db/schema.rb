699a1977386e699c6d812b79645a7df9ba407bf0
Josh Chappelow <jdchappelow@gmail.com>
2022-08-25 20:53:16 +0900

    images added to issues, and seeds, issue cards exit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_024704) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_25_103210) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_024704) do
     t.text "explanation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "pg_search_documents", force: :cascade do |t|
