4012024b914213fbe0083ae929b3a51c8eab061b
Carla <108127886+carlawlls@users.noreply.github.com>
2022-08-26 16:25:15 +0900

    Revert "added like button"

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :products, only: [:index, :show] do
-    member do
-      post "toggle_favorite", to: "products#toggle_favorite", as: :toggle_favorite
-    end
-  end
+  resources :products, only: [:index, :show]
   resources :issues, only: [:index, :show]
   get "search", to: "pages#search"
   # Defines the root path route ("/")
