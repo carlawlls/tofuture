81cf197eff6595a49061b6d4aa366af99b3dd3da
Josh Chappelow <jdchappelow@gmail.com>
2022-08-29 14:36:43 +0900

    added seeds for news, issues

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,6 @@
 class Issue < ApplicationRecord
   has_many :issue_relationships
+  has_many :news_stories
   has_many :products, through: :issue_relationships, source: :product
   include PgSearch::Model
   multisearchable against: :issue_name
