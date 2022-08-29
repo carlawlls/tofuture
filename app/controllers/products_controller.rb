f649980c5a3af319e61acfbad89e272066a599a7
Ayaka Inaba <ayakayakaaaa@gmail.com>
2022-08-29 11:09:21 +0900

    fixed the favorite button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:show, :index]
 
   def index
     @products = Product.all
