b640df0ad87d88b4847764e9f97997c3dfa22768
Ayaka Inaba <ayakayakaaaa@gmail.com>
2022-08-31 16:41:36 +0900

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,10 @@ class UsersController < ApplicationController
   def favorites
     @favorites = current_user.favorited_by_type('Product')
   end
+
+  def delete_favorites
+    @favorites = current_user.favorited_by_type('Product')
+    @favorites.remove_favorite
+    redirect_to favorites_path
+  end
 end
